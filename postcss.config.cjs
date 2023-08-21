f757e31fa1ac3ea1b100532b3a2fc2c3089139aa
bixiaocong <bicongcong@xiaomi.com>
2023-08-21 23:10:51 +0800

    提交最新

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
       fontViewportUnit: "vw", // 指定字体需要转换成的视窗单位，默认vw
       selectorBlackList: ["wrap"], // 指定不转换为视窗单位的类名，
       minPixelValue: 1, // 默认值1，小于或等于1px则不进行转换
-      mediaQuery: true, // 是否在媒体查询的css代码中也进行转换，默认false
+      mediaQuery: false, // 是否在媒体查询的css代码中也进行转换，默认false
       replace: true, // 是否转换后直接更换属性值
       exclude: [/node_modules/], // 设置忽略文件，用正则做目录名匹配
       landscape: false // 是否处理横屏情况
